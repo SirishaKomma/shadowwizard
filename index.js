209fbbe42cdddf837b31e60783eff7be3b7a2650
sireesha_k <sireesha9.k@gmail.com>
2020-10-26 18:00:04 +0530

    updated

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ function shadowizard(options){
 let images=document.querySelectorAll('.shadowizard')
 if(options.shadow_type=='hard'){
     options.shadow_type='0px'
-}else
-options.shadow_type='15px';
+}else{
+    options.shadow_type='15px';
+   
+}
 
+console.log(options.shadow_type)
 images.forEach(image=>{
     image.style.boxShadow=`10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12) `;
     if(options.padding){
